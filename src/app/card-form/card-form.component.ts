f97e8e16943b77abf11d40ae0db16dc95ad6ae85
Ismael Perez <ismaelperezemail@gmail.com>
2020-05-28 17:45:25 -0700

    chore(App): added more input fields and basic validation

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -9,6 +9,20 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class CardFormComponent implements OnInit {
   cardForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    cardNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(16),
+      Validators.maxLength(16),
+    ]),
+    expiration: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^(0[1-9] |1[0-2])\/\d{2}$/),
+    ]),
+    securityCode: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(3),
+    ]),
   });
 
   constructor() {
@@ -16,4 +30,8 @@ export class CardFormComponent implements OnInit {
   }
 
   ngOnInit(): void {}
+
+  onSubmit() {
+    console.log('FORM WAS SUBMITTED');
+  }
 }
